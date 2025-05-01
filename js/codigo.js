b1df7fc3802d956383e245d4606b6ed6ef2d3cf0
PaulRomeroSierra <romerosierrapaul@gmail.com>
2025-05-01 16:43:25 -0500

    Nuevas validaciones
    Validacion de contraseña del formulario de inicio, validacion de correo, contraseña, nombre del formulario de resgistro

--- js/codigo.js
+++ js/codigo.js
@@ -14,9 +14,19 @@ if (form) {
         validarInicio();
     });
 
+
+    /**
+     * Al menos una letra mayúscula en inglés
+Al menos una letra minúscula en inglés
+Al menos un dígito
+Al menos un carácter especial
+Mínimo de ocho pulgadas de largo
+     */
     function validarInicio() {
+        const aviso = document.querySelector(".aviso");
         let flag = false;
         let validar = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        let validarContraseña = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{5,}$/;
         if (!validar.test(mail.value)) {
             flag = true;
             cancel.textContent = "cancel";
@@ -25,15 +35,20 @@ if (form) {
             cancel.textContent = "check";
             cancel.style.color = "green";
         }
-        if (verContraseña.value.length < 6) {
+        if (!validarContraseña.test(verContraseña.value)) {
             flag = true;
             cancel_pass.textContent = "cancel";
             cancel_pass.style.color = "red";
+            aviso.style.opacity="1"
         } else {
             cancel_pass.textContent = "check";
             cancel_pass.style.color = "green";
+            aviso.style.opacity="0";
         }
         if (flag) {
+            cancel.style.bottom = "5px";
+            cancel_pass.style.bottom = "5px";
+        }else{
             cancel.style.bottom = "5px";
             cancel_pass.style.bottom = "5px";
         }
@@ -56,33 +71,80 @@ if (form_registro) {
     const ocultar1 = document.querySelector(".pass_register");
     const ocultar2 = document.querySelector(".pass_new");
     const nombre = document.getElementById("name_register");
-    const mail_register = document.getElementById("mail_register"); 
-    const constraseña1=document.querySelector("#pass_register")
+    const mail_register = document.getElementById("mail_register");
+    const constraseña1 = document.querySelector(".contraseña1_register")
     const verContraseña2 = document.querySelector(".contraseña2_register");
-    const cancel_name = document.querySelector(".cancel-name");
+    const cancel_name = document.querySelector(".cancel_user");
+    const cancel_mail = document.querySelector(".cancel_mail");
+    const cancel_new_pass = document.querySelector(".cancel_new_pass");
+    const cancel_confirm_pass = document.querySelector(".cancel_confirm_pass");
 
 
-    validarRegistro();
+    eventsRegistro();
     form_registro.addEventListener("submit", e => {
         e.preventDefault();
+        validarRegistro();
     });
-
     function validarRegistro() {
-        ocultar2.addEventListener("mousedown", e => {
-            ocultar2.innerHTML = "visibility";
-            verContraseña2.setAttribute("type", "text");
-        });
-        ocultar2.addEventListener("click", e => {
-            ocultar2.innerHTML = "visibility_off";
-            verContraseña2.setAttribute("type", "password");
-        });
-        ocultar1.addEventListener("mousedown", e => {
-            ocultar1.innerHTML = "visibility";
-            constraseña1.setAttribute("type", "text");
-        });
-        ocultar1.addEventListener("click", e => {
-            ocultar1.innerHTML = "visibility_off";
-            constraseña1.setAttribute("type", "password");
-        });
-    }
+        let flag = false;
+        let ValidarNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+        let validarMail = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        let validarContraseñaRegistro = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{5,}$/;
+        if (!ValidarNombre.test(nombre.value)) {
+            flag = true;
+            cancel_name.textContent = "cancel";
+            cancel_name.style.color = "red";
+        }else {
+            cancel_name.textContent = "check";
+            cancel_name.style.color = "green";
+        }
+        if(!validarMail.test(mail_register.value)){
+            flag= true;
+            cancel_mail.textContent="cancel";
+            cancel_mail.style.color="red";
+        }else{
+            cancel_mail.textContent="check";
+            cancel_mail.style.color="green";
+        }
+        if(!validarContraseñaRegistro.test(constraseña1.value)|| constraseña1.value != verContraseña2.value){
+            cancel_new_pass.textContent="cancel";
+            cancel_new_pass.style.color="red";
+        }else{
+            cancel_new_pass.textContent="check";
+            cancel_new_pass.style.color="green";
+
+        }
+        if(!validarContraseñaRegistro.test(verContraseña2.value) || constraseña1.value!=verContraseña2.value){
+            cancel_confirm_pass.textContent="cancel";
+            cancel_confirm_pass.style.color="red";
+        }else{
+            cancel_confirm_pass.textContent="check";
+            cancel_confirm_pass.style.color="green";
+        }
+        if (flag) {
+            cancel_name.style.bottom = "5px";
+            cancel_mail.style.bottom = "5px";
+            cancel_new_pass.style.bottom="5px";
+            cancel_confirm_pass.style.bottom="5px";
+        }
+        }
+
+        function eventsRegistro(){
+            ocultar2.addEventListener("mousedown", e => {
+                ocultar2.innerHTML = "visibility";
+                verContraseña2.setAttribute("type", "text");
+            });
+            ocultar2.addEventListener("click", e => {
+                ocultar2.innerHTML = "visibility_off";
+                verContraseña2.setAttribute("type", "password");
+            });
+            ocultar1.addEventListener("mousedown", e => {
+                ocultar1.innerHTML = "visibility";
+                constraseña1.setAttribute("type", "text");
+            });
+            ocultar1.addEventListener("click", e => {
+                ocultar1.innerHTML = "visibility_off";
+                constraseña1.setAttribute("type", "password");
+            });
+        }
 }
