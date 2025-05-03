0b716b7eb825c9fea5ee0bb9408d6ec5e0c20a95
PaulRomeroSierra <romerosierrapaul@gmail.com>
2025-05-03 17:47:03 -0500

    Validaciones de formularios parcialmente terminadas
    Nueva validacion, coincidencia de contraseña en el formulario de registro, validacion dinamica en el formulario de inicio.

--- js/codigo.js
+++ js/codigo.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#form_inicio");
 const form_registro = document.querySelector("#form_registro");
 
+//fORMULARIO DE INICIO
 if (form) {
     const ocultar = document.querySelector(".pass");
     const verContraseña = document.querySelector(".contraseña");
@@ -9,37 +10,23 @@ if (form) {
     const cancel_pass = document.querySelector(".cancel-pass");
 
     eventsIncio();
+    validacionInicio();
     form.addEventListener("submit", e => {
-        e.preventDefault();
-        validarInicio();
+        if(validarInicio()){
+            e.preventDefault();
+            alert("No puedes iniciar sin ingresar los datos");
+        }
     });
-    function validarInicio() {
-        let flag = false;
-        let validar = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
-        let validarContraseña = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{5,}$/;
-        if (!validar.test(mail.value)) {
-            flag = true;
-            cancel.textContent = "cancel";
-            cancel.style.color = "red";
-        } else {
-            cancel.textContent = "check";
-            cancel.style.color = "green";
+
+    function validarInicio(){
+        let valido =false;
+        if(validarMailInicio()){
+            valido=true;
         }
-        if (!validarContraseña.test(verContraseña.value)) {
-            flag = true;
-            cancel_pass.textContent = "cancel";
-            cancel_pass.style.color = "red";
-        } else {
-            cancel_pass.textContent = "check";
-            cancel_pass.style.color = "green";
+        if(validarContraseñaInicio()){
+        valido=true;
         }
-        if (flag) {
-            cancel.style.bottom = "5px";
-            cancel_pass.style.bottom = "5px";
-        } else {
-            cancel.style.bottom = "5px";
-            cancel_pass.style.bottom = "5px";
-        }
+        return valido;
     }
 
     function eventsIncio() {
@@ -52,8 +39,54 @@ if (form) {
             verContraseña.setAttribute("type", "password");
         });
     }
+
+    function validarMailInicio(){
+        let flag = false;
+        let validar = (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        if (!validar.test(mail.value)) {
+            flag = true;
+            cancel.textContent = "cancel";
+            cancel.style.color = "red";
+        } else {
+            cancel.textContent = "check";
+            cancel.style.color = "green";
+        }
+        if (flag) {
+            cancel.style.bottom = "5px";
+        } else {
+            cancel.style.bottom = "5px";
+        }
+        return flag;
+    }
+
+    function validarContraseñaInicio(){
+        let flag = false;
+        let validarContraseña = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{5,}$/;
+        if (!validarContraseña.test(verContraseña.value)) {
+            flag = true;
+            cancel_pass.textContent = "cancel";
+            cancel_pass.style.color = "red";
+        } else {
+            cancel_pass.textContent = "check";
+            cancel_pass.style.color = "green";
+        }
+        if (flag) {
+            cancel_pass.style.bottom = "5px";
+        } else {
+            cancel_pass.style.bottom = "5px";
+        }
+        return flag;
+    }
+
+
+    function validacionInicio(){
+        mail.addEventListener("input", validarMailInicio);
+        verContraseña.addEventListener("input",validarContraseñaInicio);
+    }
 }
 
+
+//fORMULARIO DE REGISTRO
 if (form_registro) {
     const ocultar1 = document.querySelector(".pass_register");
     const ocultar2 = document.querySelector(".pass_new");
@@ -66,18 +99,16 @@ if (form_registro) {
     const cancel_new_pass = document.querySelector(".cancel_new_pass");
     const cancel_confirm_pass = document.querySelector(".cancel_confirm_pass");
     const aviso = document.querySelector(".aviso");
-
-
+    const contraseña_aviso=document.querySelector(".aviso--1");
 
     eventsRegistro();
-    validacion();
+    validacionRegistro();
     form_registro.addEventListener("submit", e => {
         if(validarFormulario()){
             e.preventDefault();
-            alert("Completa los campos en rojo")
+            alert("No puedes registrarte sin llenar los campos correctamente");
         }
     });
-
 
     function validarFormulario(){
         let valido =false;
@@ -92,7 +123,6 @@ if (form_registro) {
         }
         return valido;
     }
-
 
     function eventsRegistro() {
         ocultar2.addEventListener("mousedown", e => {
@@ -113,16 +143,16 @@ if (form_registro) {
         });
     }
 
-    function validacion() {
+    function validacionRegistro() {
         nombre.addEventListener("input", validacionNombre);
         mail_register.addEventListener("input", validacionMail);
         constraseña1.addEventListener("input", validacionContraseñaNueva);
-        verContraseña2.addEventListener("input", validacionContraseñaNueva);
+        verContraseña2.addEventListener("input",validacionContraseñaNueva);
     }
 
     function validacionNombre() {
         let flag = false;
-        let ValidarNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+        let ValidarNombre = /^[a-zA-ZÀ-ÿ\s]{2,40}$/;
         if (!ValidarNombre.test(nombre.value)) {
             flag = true;
             cancel_name.textContent = "cancel";
@@ -159,7 +189,7 @@ if (form_registro) {
     function validacionContraseñaNueva() {
         let flag = false;
         let validarContraseñaRegistro = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{5,}$/;
-        if (!validarContraseñaRegistro.test(constraseña1.value) || constraseña1.value != verContraseña2.value) {
+        if (!validarContraseñaRegistro.test(constraseña1.value) ) {
             flag = true;
             cancel_new_pass.textContent = "cancel";
             cancel_new_pass.style.color = "red";
@@ -167,7 +197,7 @@ if (form_registro) {
             cancel_new_pass.textContent = "check";
             cancel_new_pass.style.color = "green";
         }
-        if (!validarContraseñaRegistro.test(verContraseña2.value) || constraseña1.value != verContraseña2.value) {
+        if (!validarContraseñaRegistro.test(verContraseña2.value)) {
             flag =true;
             cancel_confirm_pass.textContent = "cancel";
             cancel_confirm_pass.style.color = "red";
@@ -176,6 +206,12 @@ if (form_registro) {
             cancel_confirm_pass.textContent = "check";
             cancel_confirm_pass.style.color = "green";
             aviso.style.opacity = "0";
+        }
+        if(constraseña1.value != verContraseña2.value){
+            flag=true;
+            contraseña_aviso.style.display="block";
+        }else{
+            contraseña_aviso.style.display="none";
         }
         if (flag) {
             cancel_new_pass.style.bottom = "5px";
